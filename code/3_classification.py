40afa4cc2c906a3e62f693ac2f4f130d34429546
rossdahlke <ross.dahlke@gmail.com>
2021-01-10 15:40:37 -0800

    slurm submission script

--- code/3_classification.py
+++ code/3_classification.py
@@ -78,7 +78,7 @@ def dummy_data_collector(features):
 
 training_args = TrainingArguments(
     output_dir='./results',          # output directory
-    num_train_epochs=10,              # total # of training epochs
+    num_train_epochs=1,              # total # of training epochs
     per_device_train_batch_size=1000,  # batch size per device during training
     per_device_eval_batch_size=1000,   # batch size for evaluation
     warmup_steps=500,                # number of warmup steps for learning rate scheduler
