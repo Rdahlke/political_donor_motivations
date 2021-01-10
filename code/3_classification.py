c5c534dce5f8f067d8876dc41d8066766e7c7383
rossdahlke <ross.dahlke@gmail.com>
2021-01-10 14:53:11 -0800

    stupid emoji can't be handled by sherlock

--- code/3_classification.py
+++ code/3_classification.py
@@ -88,7 +88,7 @@ training_args = TrainingArguments(
 )
 
 trainer = Trainer(
-    model=model,                         # the instantiated 🤗 Transformers model to be trained
+    model=model,                         # the instantiated Transformers model to be trained
     args=training_args,                  # training arguments, defined above
     train_dataset=train_dataset,         # training dataset
     eval_dataset=test_dataset,           # evaluation dataset
