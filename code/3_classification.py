bf55725e92e8fa4bf770457a30be77efeb69405a
rossdahlke <ross.dahlke@gmail.com>
2021-01-10 19:31:21 -0800

    figuring out predictions

--- code/3_classification.py
+++ code/3_classification.py
@@ -11,6 +11,7 @@ import torch
 from transformers import BertForSequenceClassification, Trainer, TrainingArguments, InputFeatures, AdamW, BertTokenizer
 from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from torch.nn import functional as F
 
 # read in pre-coded data
 coded = pd.read_csv("data/posts_coded.csv")
@@ -24,10 +25,10 @@ variable_key["label"] = variable_key.index
 coded_long = coded_long.merge(variable_key)
 
 # split into train and test datasets
-trn_idx, test_idx = train_test_split(np.arange(10), test_size = .1, random_state = 1)
+trn_idx, test_idx = train_test_split(np.arange(len(coded_long)), test_size = .5, random_state = 1)
 
 # load in the large BERT model
-model = BertForSequenceClassification.from_pretrained("bert-large-uncased", num_labels = len(variable_key))
+model = BertForSequenceClassification.from_pretrained("bert-large-uncased")
 
 # loving that cuda
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -43,7 +44,7 @@ optimizer_grouped_parameters = [
 ]
 optimizer = AdamW(optimizer_grouped_parameters, lr = 1e-5)
 
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+tokenizer = BertTokenizer.from_pretrained("bert-large-uncased")
 
 # preparing training batch
 train_batch = [coded_long["text"][i] for i in trn_idx]
@@ -79,8 +80,8 @@ def dummy_data_collector(features):
 training_args = TrainingArguments(
     output_dir='./results',          # output directory
     num_train_epochs=1,              # total # of training epochs
-    per_device_train_batch_size=1,  # batch size per device during training
-    per_device_eval_batch_size=1,   # batch size for evaluation
+    per_device_train_batch_size=1000,  # batch size per device during training
+    per_device_eval_batch_size=1000,   # batch size for evaluation
     warmup_steps=500,                # number of warmup steps for learning rate scheduler
     weight_decay=0.01,               # strength of weight decay
     logging_dir='./logs',
@@ -101,4 +102,82 @@ torch.cuda.empty_cache()
 
 trainer.evaluate()
 
-print(model(test_input_ids, test_attention_mask, labels = test_labels))
+print((model(test_input_ids, test_attention_mask, labels = test_labels)))
+
+output = model(test_input_ids, test_attention_mask)
+
+_, prediction = torch.max(output, dim = 1)
+
+torch.max(F.softmax(output[0]), dim = 1)
+
+output.preds
+
+F.softmax(model.predict(test_dataset))
+
+output
+
+_, prediction = torch.max(output, dim=1)
+
+prediction
+
+
+
+def get_predictions(model, text_batch, input_ids, attention_mask, labels):
+  model = model.eval()
+  texts = []
+  predictions = []
+  prediction_probs = []
+  real_values = []
+  with torch.no_grad():
+    for i in range(len(text_batch)):
+      texts = text_batch
+      input_ids = input_ids.to(device)
+      attention_mask = attention_mask.to(device)
+      targets = labels.to(device)
+      outputs = model(
+        input_ids = input_ids,
+        attention_mask = attention_mask
+      )
+      _, preds = torch.max(outputs, dim=1)
+      review_texts.extend(texts)
+      predictions.extend(preds)
+      prediction_probs.extend(outputs)
+      real_values.extend(targets)
+  predictions = torch.stack(predictions).cpu()
+  prediction_probs = torch.stack(prediction_probs).cpu()
+  real_values = torch.stack(real_values).cpu()
+  return review_texts, predictions, prediction_probs, real_values
+
+y_review_texts, y_pred, y_pred_probs, y_test = get_predictions(
+    model,
+    test_batch,
+    test_input_ids,
+    test_attention_mask,
+    test_labels
+)
+
+def get_predictions(model, text_batch, input_ids, attensio):
+  model = model.eval()
+  texts = []
+  predictions = []
+  prediction_probs = []
+  real_values = []
+  with torch.no_grad():
+    for i in range(len(test_dataset)):
+      texts = test_batch[0]
+      input_ids = test_dataset[0][0].to(device)
+      attention_mask = test_dataset[0][1].to(device)
+      targets = test_dataset[0][2].to(device)
+      outputs = model(
+        input_ids=input_ids,
+        attention_mask=attention_mask
+      )
+      _, preds = torch.max(outputs)
+      review_texts.extend(texts)
+      predictions.extend(preds)
+      prediction_probs.extend(outputs)
+      real_values.extend(targets)
+  predictions = torch.stack(predictions).cpu()
+  prediction_probs = torch.stack(prediction_probs).cpu()
+  real_values = torch.stack(real_values).cpu()
+  return review_texts, predictions, prediction_probs, real_values
