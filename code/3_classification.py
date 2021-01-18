7cfaa56c3a4f9a8d109a74b7f735fd9b77cd2ada
rossdahlke <ross.dahlke@gmail.com>
2021-01-18 14:34:53 -0800

    model runs successfully, upping to 1000 epochs

--- code/3_classification.py
+++ code/3_classification.py
@@ -85,10 +85,10 @@ def data_collector(features):
 
 training_args = TrainingArguments(
     output_dir='./results',          # output directory
-    num_train_epochs=2,              # total # of training epochs
+    num_train_epochs=1000,              # total # of training epochs
     per_device_train_batch_size=1,  # batch size per device during training
     per_device_eval_batch_size=1,   # batch size for evaluation
-    warmup_steps=100,                # number of warmup steps for learning rate scheduler
+    warmup_steps=500,                # number of warmup steps for learning rate scheduler
     weight_decay=0.01,               # strength of weight decay
     logging_dir='./logs',
     save_total_limit=1,
